ea68f1e4e32087260538278be278433d164c72d0
Paulo Cesar Ferreira de Mello <pcfmello@gmail.com>
2019-04-13 15:29:11 -0300

    Application is done

---
+++ layouts/partials/nav.vue
@@ -0,0 +1,54 @@
+<template>
+  <header>
+    <nuxt-link to="/" class="logo">Nuxt First App</nuxt-link>
+    <nav>
+      <ul>
+        <li>
+          <nuxt-link to="/">Home</nuxt-link>
+        </li>
+        <li>
+          <nuxt-link to="about">About</nuxt-link>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+header {
+  background-color: rgb(0, 219, 146);
+  display: grid;
+  grid-template-columns: repeat(2, auto);
+  padding: 8px;
+}
+
+.logo,
+li a {
+  padding: 1em 2em;
+  display: block;
+  text-transform: uppercase;
+  text-decoration: none;
+  font-weight: bold;
+  color: white;
+  font-size: 0.9em;
+}
+
+nav {
+  justify-self: right;
+}
+
+ul {
+  list-style-type: none;
+}
+li {
+  display: inline;
+}
+li a {
+  display: inline-block;
+  background: rgba(0, 0, 0, 0.1);
+}
+</style>
---
+++ pages/about.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="container">
+    <h1>About our Nuxt</h1>
+    <p class="subheading">They're juicy and stuff.</p>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  </section>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "About Nuxt First App",
+      meta: [
+        { name: "description", content: "This is my about description here." },
+        { name: "keywords", content: "about nuxt, nuxt info" }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+</style>
